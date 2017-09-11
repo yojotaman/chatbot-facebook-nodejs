ab730f6f85e8a0da57c5ed8b129b3bcb6b903bc7
yojotaman <yojotaman@gmail.com>
2017-09-10 22:52:32 -0500

    Uploading code to github

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAISBPch2B8BADoBYbarhnIz2pqEtTf8YSyy1ulkaVmMMze1hZBPvJI7E1TZBdp96os8XQTLLoClsaB2WCusYujBamZAWKeTFFzCple2Dqlnitoyr0fl9RyJjFMqSJa5jiziuxpG1RhRWSEGNees84o5ee0HyOJBLpK9ZBUy1QZDZD',
+    FB_VERIFY_TOKEN: '12345AOSABCD',
+    API_AI_CLIENT_ACCESS_TOKEN: 'd29060dd591f4e2e91385c2e1bdcd979',
+    FB_APP_SECRET: 'd769a536dfe4db4816e3dd1755f7d026',
+    SERVER_URL: "https://thawing-river-29460.herokuapp.com/",
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'yojotaman@gmail.com',
+    EMAIL_TO: 'yojotaman@gmail.com'
+};
